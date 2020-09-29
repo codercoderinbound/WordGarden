c37947b0c1164bcf615d6a96d467b9d35761bc88
Anthony Lundberg <tonylundberg12@gmail.com>
2020-09-29 14:49:49 -0700

    half of 3.7 completed

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var wordsGuessedLabel: UILabel!
@@ -19,14 +20,147 @@ class ViewController: UIViewController {
     @IBOutlet weak var gameStatusMessageLabel: UILabel!
     @IBOutlet weak var flowerImageView: UIImageView!
     
+    var wordsToGuess = ["SWIFT", "DOG", "CAT"]
+    var currentWordIndex = 0
+    var wordToGuess = ""
+    var lettersGuessed = ""
+    let maxNumberOfWrongGuesses = 8
+    var wrongGuessesRemaining = 8
+    var wordsGuessedCount = 0
+    var wordsMissedCount = 0
+    var guessCount = 0
+    var audioPlayer: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        let text = guessedLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+        wordToGuess = wordsToGuess[currentWordIndex]
+        wordBeingRevealedLabel.text = "_" + String(repeating: " _", count: wordToGuess.count - 1)
+        updateGameStatusLabels()
+        }
 
-    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+    func updateUIAfterGuess() {
+        guessedLetterTextField.resignFirstResponder()
+        guessedLetterTextField.text = ""
+        guessLetterButton.isEnabled = false
     }
+    
+    func formatRevealedWord() {
+        var revealedWord = ""
+        for letter in wordToGuess {
+            if lettersGuessed.contains(letter) {
+                revealedWord = revealedWord + "\(letter) "
+            } else {
+                revealedWord = revealedWord + "_ "
+            }
+        }
+        revealedWord.removeLast()
+        wordBeingRevealedLabel.text = revealedWord
+    }
+    
+    func updateAfterWinOrLose() {
+        //what do we do if game is over
+        //increment current word index by 1
+        //disable guessALetterTextField
+        //disable guess a letter button
+        //set playAgainButton .isHidden to false
+        //update all labels at the top of the screen
+        
+        currentWordIndex += 1
+        guessedLetterTextField.isEnabled = false
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = false
+        
+        updateGameStatusLabels()
+    }
+    
+    func updateGameStatusLabels() {
+        wordsGuessedLabel.text = "Word Guessed: \(wordsGuessedCount)"
+        wordsMissedLabel.text = "Word Guessed: \(wordsMissedCount)"
+        wordsRemainingLabel.text = "Words Remaining: \(wordsToGuess.count - (wordsGuessedCount + wordsMissedCount))"
+        wordsInGameLabel.text = "Words in Game: \(wordsToGuess.count)"
+    }
+    
+    func guessALetter () {
+        // get current letter and add it to all lettersGuessed
+//      or  let currentLetterGuessed = guessedLetterTextField.text ?? ""
+        let currentLetterGuessed = guessedLetterTextField.text!
+        lettersGuessed = lettersGuessed + currentLetterGuessed
+        
+        // format and show revealedWord in wordBeingRevealedLabel to include bew guess
+        formatRevealedWord()
+        
+        // update image, if needed, and keep track of wrong guesses
+        if wordToGuess.contains(currentLetterGuessed) == false {
+            wrongGuessesRemaining = wrongGuessesRemaining - 1
+            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
+        }
+        
+        // update gameStatusMessageLabel
+        guessCount += 1
+//        var guesses = "Guesses"
+//        if guessCount == 1 {
+//            guesses = "Guess"
+//        }
+        let guesses = (guessCount == 1 ? "Guess" : "Guesses")
+        gameStatusMessageLabel.text = "You've Made \(guessCount) \(guesses)"
+        
+        // after each guess, check to see if the user has won (all letters are guessed) or the user lost(guesses remaining are 0)
+        if wordBeingRevealedLabel.text!.contains("_") == false {
+            gameStatusMessageLabel.text = "You guessed it! It took you \(guessCount) guesses to guess the word.)"
+            wordsGuessedCount += 1
+            updateAfterWinOrLose()
+        } else if wrongGuessesRemaining == 0 {
+            gameStatusMessageLabel.text = "So sorry. You're all out of guesses."
+            wordsMissedCount += 1
+            updateAfterWinOrLose()
+        }
+        if currentWordIndex == wordToGuess.count {
+            gameStatusMessageLabel.text! += "\n\nYou've tried all of the words! Restart from the beginning?"
+        }
+        
+        }
+        
+    
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        sender.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces).uppercased()
+        guessLetterButton.isEnabled = !(sender.text!.isEmpty)
+    }
+    
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessALetter()
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        guessALetter()
+        updateUIAfterGuess()
+    }
+    
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        if currentWordIndex == wordToGuess.count {
+            currentWordIndex = 0
+            wordsGuessedCount = 0
+            wordsMissedCount = 0
+            
+        }
+        
+        playAgainButton.isHidden = true
+        guessedLetterTextField.isEnabled = true
+        guessLetterButton.isEnabled = false
+        wordToGuess = wordsToGuess[currentWordIndex]
+        wrongGuessesRemaining = maxNumberOfWrongGuesses
+        wordBeingRevealedLabel.text = "_" + String(repeating: " _", count: wordToGuess.count - 1)
+        guessCount = 0
+        flowerImageView.image = UIImage(named: "flower\(maxNumberOfWrongGuesses)")
+        lettersGuessed = ""
+        updateGameStatusLabels()
+        gameStatusMessageLabel.text = "You've Made Zero Guesses"
+        
     }
     
 }
