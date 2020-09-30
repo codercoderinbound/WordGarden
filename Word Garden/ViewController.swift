a94c45c79dc14577bec905278a03504b62640a6c
Anthony Lundberg <tonylundberg12@gmail.com>
2020-09-29 21:29:26 -0700

    3.8 complete

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -83,20 +83,47 @@ class ViewController: UIViewController {
         wordsInGameLabel.text = "Words in Game: \(wordsToGuess.count)"
     }
     
+    func drawFlowerAndPlaySound (currentLetterGuessed: String) {
+        // update image, if needed, and keep track of wrong guesses
+        if wordToGuess.contains(currentLetterGuessed) == false {
+            wrongGuessesRemaining = wrongGuessesRemaining - 1
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                UIView.transition(with: self.flowerImageView,
+                                  duration: 0.5,
+                                  options: .transitionCrossDissolve,
+                                  animations: { [self] in flowerImageView.image = UIImage(named: "wilt\(wrongGuessesRemaining)")})
+                { (_) in
+                    
+                    if self.wrongGuessesRemaining != 0 {
+                        self.flowerImageView.image = UIImage(named: "flower\(self.wrongGuessesRemaining)")
+                    } else {
+                        self.playSound(name: "word-not-guessed")
+                        UIView.transition(with: self.flowerImageView, duration: 0.5, options: .transitionCrossDissolve, animations: {self.flowerImageView.image = UIImage(named: "flower\(self.wrongGuessesRemaining)")}, completion: nil)
+                    }
+                    
+                    
+                    
+                    
+                }
+                 
+                self.playSound(name: "incorrect")
+            }
+            
+
+        } else {
+            playSound(name: "correct")
+        }
+    }
+    
     func guessALetter () {
         // get current letter and add it to all lettersGuessed
 //      or  let currentLetterGuessed = guessedLetterTextField.text ?? ""
         let currentLetterGuessed = guessedLetterTextField.text!
         lettersGuessed = lettersGuessed + currentLetterGuessed
-        
-        // format and show revealedWord in wordBeingRevealedLabel to include bew guess
+
         formatRevealedWord()
-        
-        // update image, if needed, and keep track of wrong guesses
-        if wordToGuess.contains(currentLetterGuessed) == false {
-            wrongGuessesRemaining = wrongGuessesRemaining - 1
-            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
-        }
+        drawFlowerAndPlaySound(currentLetterGuessed: currentLetterGuessed)
         
         // update gameStatusMessageLabel
         guessCount += 1
@@ -111,6 +138,7 @@ class ViewController: UIViewController {
         if wordBeingRevealedLabel.text!.contains("_") == false {
             gameStatusMessageLabel.text = "You guessed it! It took you \(guessCount) guesses to guess the word.)"
             wordsGuessedCount += 1
+            playSound(name: "word-guessed")
             updateAfterWinOrLose()
         } else if wrongGuessesRemaining == 0 {
             gameStatusMessageLabel.text = "So sorry. You're all out of guesses."
@@ -120,9 +148,21 @@ class ViewController: UIViewController {
         if currentWordIndex == wordToGuess.count {
             gameStatusMessageLabel.text! += "\n\nYou've tried all of the words! Restart from the beginning?"
         }
-        
         }
         
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(error.localizedDescription). Could not initialize AVAudioPlayer object.")
+            }
+        } else {
+            print("ERROR: Could not read data from file \(name)")
+        }
+        }
+    
     
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
